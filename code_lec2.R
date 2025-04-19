7fde92addf6a83a2982559d35059f0d939933ee6
dmuraka <45805390+dmuraka@users.noreply.github.com>
2025-04-19 22:10:02 +0900

    Update code_lec2.R

--- code_lec2.R
+++ code_lec2.R
@@ -1,5 +1,5 @@
 
-install.packages(c("sf","mapview","RColorBrewer","stringr","car")) # 初回のみ
+# install.packages(c("sf","mapview","RColorBrewer","stringr","car")) # 初回のみ#を外して回してください
 library(sf)
 library(mapview)
 library(RColorBrewer)
@@ -8,16 +8,16 @@ library(car)
 
 #######################################################
 ####################################################### データの読込・整備
-
-#####https://nlftp.mlit.go.jp/
+##### 出典：国土数値情報情報ダウンロードサービス（https://nlftp.mlit.go.jp/）
 #stationの定義：https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N05-2023.html
 #landの定義　 ：https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-L03-a-2021.html
 #priceの定義　：https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-L01-2025.html
+
 dstation<-st_read(dsn="https://www.dropbox.com/scl/fi/uqg0hzpqs6mya1hcgggcw/station_R3.geojson?rlkey=p0oixglii01xcgwuww3mcbw6x&dl=1")
 dland   <-st_read(dsn="https://www.dropbox.com/scl/fi/eb9cbb9tt2g06wydqilnl/landuse_R3.geojson?rlkey=r4bh5aoemjf3lj5ur8df6crs5&dl=1")
 dprice  <-st_read(dsn="https://www.dropbox.com/scl/fi/hllfqvyqna7eoj2y9rwem/landprice_R7.geojson?rlkey=c998opyiwbift11vmuls1f7tk&dl=1")
 
-########## 名前の変更
+########## 列名の変更
 dland[1:3,]
 names(dland)
 names(dland)<-c("mesh","rice","agri","forest","wild","building","road","rail","other","river","beach","ocean","golf","out","geometry")
@@ -28,23 +28,21 @@ names(dprice)[ names(dprice) =="L01_008" ]<-"price"
 names(dprice)[ names(dprice) =="L01_028" ]<-"status"
 names(dprice)
 
-###### 地図を見てみる（dpriceから住宅地公示地価だけ抽出->dprice2）
-# stringr: https://kazutan.github.io/kazutanR/stringr-intro.html
-
+########## 地図を確認（dpriceから住宅地公示地価だけ抽出->dprice2）
 ###### mapview
 mapview(dstation,cex=0.5)
 mapview(dland[,"building"])
 mapview(dprice[,"price"])
 
-###### sfのplot
+###### sfのplot関数の場合
 xlim <- c(139,140)
 ylim <- c(35.3,36)
 plot(st_geometry(dstation),axes=TRUE,
-     xlim=xlim,ylim=ylim,asp = NA)#asp=NAとするとアスペクト比を自由に
+     xlim=xlim,ylim=ylim,asp = NA)#asp=NAとするとアスペクト比が自由に
 plot(dland[,"building"],axes=TRUE,xlim=xlim,ylim=ylim, asp=NA )
 plot(dprice[,"price"],axes=TRUE,xlim=xlim,ylim=ylim, asp=NA )
 
-###### データを見てみる（dpriceから住宅地公示地価だけ抽出->dprice2）
+###### 属性データ（dpriceから"住宅地"公示地価だけを抽出->dprice2）
 dprice[1:10,c("price","status")]
 table(dprice$status)
 is_residual <- str_detect(dprice$status, "住宅")#部分一致
@@ -57,20 +55,22 @@ plot(dprice2[,"price"],axes=TRUE,xlim=xlim,ylim=ylim, asp=NA )
 dim(dprice)
 dim(dprice2)
 
-###### 余談：sf -> data.frame
-#coords <-st_coordinates(st_centroid(price))
-coords  <-st_coordinates(dprice2)
-price   <-dprice2$price#or price[,"price"]
+###### 余談：投影法の定義と変換
+coords   <-st_coordinates(dprice2)
+price    <-dprice2$price#or price[,"price"]
 dprice2b <-data.frame(coords,price)
 dprice2b[1:4,]
-#dprice2c <- st_as_sf(dprice2b, coords=c("X","Y"))
-#dprice2c[1:4,]
-#st_crs( d_s ) <- 4326
+dprice2c <- st_as_sf(dprice2b, coords=c("X","Y"))
+dprice2c[1:4,]
+st_crs( dprice2c ) <- 4326
+dprice2c[1:4,]
+st_crs( dprice2c ) <- 6677
+dprice2c[1:4,]
 
 #######################################################
 ####################################################### データ処理
 
-#################### 土地利用グリッドに対する処理
+#################### 住宅地公示地価データに対する処理
 
 ###### 土地利用データの空間結合
 #https://bakaniko.github.io/FOSS4G2019_Geoprocessing_with_R_workshop/geoprocessing-in-r.html
@@ -100,44 +100,30 @@ dland$tk_dist <- apply(dmat_t, 1, min)
 
 ###### データの性質の確認
 summary(dprice3)
-dprice3 <- na.omit(dprice3)  # 欠損を含むデータの除去
-price   <- dprice3$price
-st_dist <- dprice3$st_dist
-tk_dist <- dprice3$tk_dist
+dprice3 <- na.omit(dprice3)  # 欠損を含む行の除去
+price   <- dprice3$price     # 住宅地公示地価
+st_dist <- dprice3$st_dist   # 最寄駅までの距離
+tk_dist <- dprice3$tk_dist   # 東京駅までの距離
 
-hist(price)
-hist(log(price))
-
-plot(log(price),log(st_dist))
-plot(log(price),log(tk_dist))
+plot(log(price),log(st_dist))# 住宅地価と最寄駅距離の2次元プロット
+plot(log(price),log(tk_dist))# 住宅地価と東京駅までの距離の2次元プロット
 
 ###### 回帰モデルの推定
-mod     <- lm(log(price)~log(st_dist)+log(tk_dist)+rice+agri+forest+wild+building+road+rail+other+river+ocean+golf,data=dprice3)
-summary(mod)# 結果
-vif(mod)    # 多重共線性の確認
-
-###### 回帰モデル（変数選択後）の推定
 mod     <- lm(log(price)~log(st_dist)+log(tk_dist)+rice+agri+forest+wild+building+road+rail+river,data=dprice3)
 summary(mod)# 結果
-vif(mod)    # 多重共線性の確認
 
 ###### 予測値（対数スケール）
-log_pred<- predict(mod)
-plot(log(dprice3$price),log_pred)
-abline(0,1,col="red")
-
-###### 予測値
-pred    <- exp(predict(mod))
-plot(dprice3$price,pred)
-abline(0,1,col="red")
+log_pred<- predict(mod)          # 予測値
+plot(log(dprice3$price),log_pred)# 対数スケールでの実測値と予測値の比較
+abline(0,1,col="red")            # 45度線プロットを引く
 
 #######################################################
 ####################################################### 土地利用グリッド毎の地価予測
 
 ###### 陸地を含むグリッドのみを選択 -> dland2
-land_dat1<-st_drop_geometry(dland[,c(2:14)]) # geometryを除去（sf->data.frame）
-land_dat2<-rowSums(land_dat1[,-11])# 海(ocean)以外の土地利用の面積
-dland2  <-dland[land_dat2>0,]      # 海(ocean)以外の土地利用が0以上のグリッド
+land_dat1<-st_drop_geometry(dland[,c(2:14)]) # 土地利用に関する変数の列だけ取り出してgeometryを除去（sf->data.frame）
+land_dat2<-rowSums(land_dat1[,-11])          # 海(ocean)以外の土地利用面積
+dland2  <-dland[land_dat2>0,]                # 海(ocean)以外の土地利用が0以上のグリッド
 
 ###### 住宅地価の予測 -> dland2$pred
 log_pred0      <- predict(mod,newdata=dland2)
